162bc22a29ad08a1da335d8d27afd2a4b78b182a
Laurent <laurent.sem@edu.itescia.fr>
2019-12-04 09:36:13 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/main/java/org/jmll/imagefilter/Filters/Filters.java
+++ src/main/java/org/jmll/imagefilter/Filters/Filters.java
@@ -1,8 +1,29 @@
 package org.jmll.imagefilter.Filters;
 
+import javafx.scene.effect.GaussianBlur;
+import org.bytedeco.opencv.global.opencv_imgproc;
+import org.bytedeco.opencv.opencv_core.Size;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.jmll.imagefilter.FiltersException;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 
 public class Filters {
+
+    public Filters {
+
+    }
+
+    public Mat toBlur (Mat image) throws FiltersException {
+        try {
+            int size = 3;
+            Mat cloneImage = image.clone();
+            opencv_imgproc.GaussianBlur(image, cloneImage, new Size(size, size), 0);
+
+        } catch {
+
+        }   return cloneImage;
+
+    }
 }
---
+++ src/main/java/org/jmll/imagefilter/FiltersException.java
@@ -0,0 +1,16 @@
+package org.jmll.imagefilter;
+
+public class FiltersException extends Exception {
+
+        public FiltersException(String message) {
+            super(message);
+        }
+
+        public FiltersException(String message, Throwable t) {
+            super(message, t);
+        }
+
+
+   }
+
+
