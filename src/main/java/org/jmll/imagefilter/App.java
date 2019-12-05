fb90f7b6d59c0d6e3cdc7ef0f4492b3b5eecd8d4
Maxence Lavenu <maxence.lavenu@outlook.fr>
2019-12-05 13:23:10 +0100

    Working .ini file with CLI interface

--- src/main/java/org/jmll/imagefilter/App.java
+++ src/main/java/org/jmll/imagefilter/App.java
@@ -1,19 +1,18 @@
 package org.jmll.imagefilter;
 
 import org.apache.commons.cli.*;
-
 import org.bytedeco.opencv.opencv_core.Mat;
+import org.ini4j.Wini;
 import org.jmll.imagefilter.Filters.*;
-
-import static org.bytedeco.opencv.global.opencv_imgcodecs.imread;
-import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
-
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imread;
+import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
 
 public class App {
 
@@ -25,9 +24,11 @@ public class App {
 
         // print given arguments to console
         System.out.println("Current arguments: " + Arrays.toString(args));
+        System.out.println();
 
         // create Options object
         Options options = new Options();
+
 
         // add t option
         options.addOption(new Option("h", "help", false, "Help"));
@@ -40,6 +41,13 @@ public class App {
         //Create the list that will contain the applied filters
         ArrayList<Filter> filtersList = new ArrayList<>();
 
+        //Create the default values for the arguments
+        File inputDir = new File("imgs");
+        App.logFilename = "image.log";
+        File outputDir = new File("output");
+        String filters = null;
+
+
         // Invokes the parser for the parameters
         CommandLineParser parser = new DefaultParser();
         CommandLine cmd = null;
@@ -49,24 +57,102 @@ public class App {
             e.printStackTrace();
         }
 
-        //Define the input directory
-        App.logFilename = new String(cmd.getOptionValue("lf"));
-        Logger.log("Log file filename: " + App.logFilename, true);
+
+        // FROM INI FILE
+        System.out.println("Reading from the ini file...");
+
+        // Opening .ini file
+        Wini ini = null;
+        try {
+            ini = new Wini(new File(cmd.getOptionValue("cf")));
+
+            //Define the log file filename
+            try {
+                App.logFilename = new String(ini.get("general", "logFile"));
+                Logger.log("Log file filename: " + App.logFilename, true);
+            } catch (NullPointerException e) {
+                System.out.println("Log file filename not found. Proceeding with default name: image.log");
+                e.printStackTrace();
+            }
+
+            //Define the input directory
+            try {
+                inputDir = new File(ini.get("general", "inputDir"));
+                Logger.log("Input directory: " + inputDir + "/", true);
+            } catch (NullPointerException e) {
+                System.out.println("Input directory not found. Proceeding with default name: imgs/");
+                e.printStackTrace();
+            }
+
+            //Define the output directory
+            try {
+                outputDir = new File(ini.get("general", "outputDir"));
+                Logger.log("Output directory: " + outputDir + "/", true);
+
+            } catch (NullPointerException e) {
+                System.out.println("Output directory not found. Proceeding with default name: output/");
+                e.printStackTrace();
+            }
+
+            //Define the filters
+            try {
+                filters = new String(ini.get("filters", "content"));
+                Logger.log("Filters: " + filters, true);
+
+            } catch (NullPointerException e) {
+                System.out.println("Filters not found.");
+                e.printStackTrace();
+            }
+
+        } catch (NullPointerException e) {
+            System.out.println(".ini file not found.");
+        } catch (IOException e) {
+            System.out.println(".ini file not found.");
+        }
+
+        // basic line print to space out the ini file information and the parameters
+        System.out.println();
+
+        // FROM PARAMETERS
+        Logger.log("Reading from the Java parameters...", true);
+
+        //Define the log file filename
+        try {
+            App.logFilename = new String(cmd.getOptionValue("lf"));
+            Logger.log("Log file file name: " + App.logFilename, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         //Define the input directory
-        File inputDir = new File(cmd.getOptionValue("i"));
-        Logger.log("Input directory: " + inputDir + "/", true);
+        try {
+            inputDir = new File(cmd.getOptionValue("i"));
+            Logger.log("Input directory: " + inputDir + "/", true);
+        } catch (NullPointerException e) {
+            System.out.println("Input directory not found. Proceeding with: " + inputDir + "/");
+        }
 
         //Define the output directory
-        File outputDir = new File(cmd.getOptionValue("o"));
-        Logger.log("Output directory: " + outputDir + "/", true);
+        try {
+            outputDir = new File(cmd.getOptionValue("o"));
+            Logger.log("Output directory: " + outputDir + "/", true);
+        } catch (NullPointerException e) {
+            System.out.println("Output directory not found. Proceeding with: " + outputDir + "/");
+        }
 
+        try {
+            filters = new String(cmd.getOptionValue("f"));
+            Logger.log("Filters: " + filters, true);
+
+        } catch (NullPointerException e) {
+            System.out.println("Filters not found.");
+        }
 
         // basic line print to space out the parameters information and the image treatment prints
         System.out.println();
 
         // parse filters
-        String filterArg = cmd.getOptionValue("f"); // grayscale|blur
+        String filterArg = filters; // grayscale|blur
 
         String[] split = filterArg.split("\\|"); // blur, grayscale
         for (String s : split) {
@@ -93,7 +179,7 @@ public class App {
         }
 
         // File est dossier qui contient les images
-        String directoryName = cmd.getOptionValue("i");
+        String directoryName = inputDir.toString();
         File dir = new File(directoryName + "/");
 
         if (dir.isDirectory()) {
