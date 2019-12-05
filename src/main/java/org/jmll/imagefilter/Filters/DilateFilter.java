0577d77ee5e3b8eae1e8992ae4f012ad03629a1a
Jonathan <jonathandebailleux@yahoo.fr>
2019-12-05 10:37:28 +0100

    Exception DilateFilter

--- src/main/java/org/jmll/imagefilter/Filters/DilateFilter.java
+++ src/main/java/org/jmll/imagefilter/Filters/DilateFilter.java
@@ -27,9 +27,14 @@ public class DilateFilter extends Filter {
     @Override
     public Mat process(Mat image) throws FilterException {
         Mat cloneImage = image.clone();
+        try{
         Mat element = opencv_imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2 * size + 1, 2 * size + 1));
         opencv_imgproc.dilate(image, cloneImage, element);
-        Logger.log("Adding dilate filter with size = " + size, true);
+        Logger.log("Adding dilate filter with size = " + size, true);}
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
 
         return cloneImage;
     }
